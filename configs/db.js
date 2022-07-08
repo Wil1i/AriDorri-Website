d74cefef7ce970ded75c112901e7557beb3bfdb0
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-07-08 17:24:18 +0430

    configs for database moved to config file

--- configs/db.js
+++ configs/db.js
@@ -1,6 +1,7 @@
 const sequelize = require('sequelize');
+const config = require("./config.json")
 
-const db = new sequelize('willi', 'root', 'shayanwilliams', {
+const db = new sequelize(config.db.name, config.db.username, config.db.password, {
     host: 'localhost',
     dialect: 'mysql',
 });
