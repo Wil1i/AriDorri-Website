cf79dd50cca74a83a09333b8631ba29899ff240b
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-07-15 17:31:16 +0430

    404 ERROR added and error page designed

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ const suggestionsHandler = require("../controllers/suggestionsHandler")
 Router.get("/suggestions", suggestionsHandler.get)
 Router.post("/suggestions", suggestionsHandler.post)
 
+Router.get("*", (req, res) => {
+    res.render("error", {code : 404, msg : "Page not found"})
+})
+
 module.exports = Router
