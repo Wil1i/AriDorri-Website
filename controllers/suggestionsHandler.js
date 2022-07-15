8d5c5a4c8718c75ddb4ae5d124211265f075930e
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-07-15 17:07:50 +0430

    Optimized and bug fix

--- controllers/suggestionsHandler.js
+++ controllers/suggestionsHandler.js
@@ -10,26 +10,28 @@ const get = async (req, res) => {
 }
 
 const post = async (req, res) => {
+    console.log("Start sending request")
     const suggestion = await Suggest.findByPk(req.query.id)
+    console.log(`The suggestion is`, suggestion)
     if(suggestion && req.body.mode == 'accept'){
-        axios.post("http://localhost:3000/sendMsg", {
-            id : suggestion.disId
-        }).then(res => {
-            if(req.data.status == "sent"){
+        console.log("Mode is accept. Sending...")
+        await axios.post(`http://localhost:3000/sendMsg?id=${suggestion.disId}`, {test : 1}).then(result => {
+            if(result.data['status'] == "sent"){
                 req.flash("success", "Message successfully sent")
                 res.redirect("/suggestions")
             }else{
                 req.flash("danger", "Can't send message to this user")
                 res.redirect("/suggestions")
             }
-        }).catch(error => {})
+        }).catch(error => {
+            console.log("Error!", error)
+        })
     }else if(suggestion && req.body.mode == 'declined'){
+        console.log("Mode is decline, deleting...")
         suggestion.destroy()
         req.flash("success", "Successfully deleted suggestion.")
         res.redirect("/suggestions")
     }
-
-    res.redirect("/suggestions")
 }
 
 module.exports = {
