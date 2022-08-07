0ae8ddce7688080c6faf286d558bd6790c6d9ea2
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-08-07 19:47:07 +0430

    Last change

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const Router = new express.Router()
 
+const homePageController = require("../controllers/homePageController")
+Router.get("/", homePageController.get)
+
 const suggestionsHandler = require("../controllers/suggestionsHandler")
 Router.get("/suggestions", suggestionsHandler.get)
 Router.post("/suggestions", suggestionsHandler.post)
