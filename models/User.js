074db696e17d84d3f981e3476d33ea8a5afcf8da
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-08-09 22:47:27 +0430

    Functions for manage user password added

--- models/User.js
+++ models/User.js
@@ -1,32 +1,48 @@
-const { DataTypes } = require("sequelize")
-const db = require("../configs/db")
+const { DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const db = require("../configs/db");
 
-const User = db.define("users", {
-    id : {
-        type : DataTypes.NUMBER,
-        primaryKey : true,
-        autoIncrement : true
+const User = db.define(
+  "users",
+  {
+    id: {
+      type: DataTypes.NUMBER,
+      primaryKey: true,
+      autoIncrement: true,
     },
 
-    username : {
-        type : DataTypes.TEXT
+    username: {
+      type: DataTypes.TEXT,
     },
 
-    discordUsername : {
-        type : DataTypes.TEXT
+    discordUsername: {
+      type: DataTypes.TEXT,
     },
 
-    userRank : {
-        type : DataTypes.TEXT
+    userRank: {
+      type: DataTypes.TEXT,
     },
 
-    warn : {
-        type : DataTypes.TEXT
-    }
-}, {
-    timestamps : false
-})
+    warn: {
+      type: DataTypes.TEXT,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+User.validPassword = function (user, password) {
+  return bcrypt.compareSync(password, user.password);
+};
+
+User.encryptPassword = async (password) => {
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = await bcrypt.hashSync(password, salt);
+  return hash;
+};
 
 module.exports = {
-    User
-}
+  User,
+};
