f6c378639a8458080940d082935c6307b894d695
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-08-17 02:55:54 +0430

    Register system added

---
+++ controllers/registerController.js
@@ -0,0 +1,41 @@
+const User = require("../models/User");
+
+const get = async (req, res) => {
+  res.render("register", {
+    flash: req.flash(),
+    user: req.user,
+  });
+};
+
+const post = async (req, res) => {
+  // ! This function have a problem (findone)
+  const isUserExist =
+    (await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    })) || false;
+
+  if (isUserExist) {
+    req.flash("danger", "با مشخصات وارد شده قادر به ساخت اکانت نیستید");
+    res.redirect("/register");
+  } else {
+    await User.create({
+      username: req.body.username,
+      password: await User.encryptPassword(req.body.password),
+      number: req.body.number,
+      userRank: "user",
+    });
+
+    req.flash(
+      "success",
+      "اکانت شما با موفقیت ساخته شد. شما میتوانید هم اکنون به اکانت خود وارد شوید."
+    );
+    res.redirect("/login");
+  }
+};
+
+module.exports = {
+  get,
+  post,
+};
--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,10 @@ const loginController = require("../controllers/loginController");
 Router.get("/login", isUserNotLoggedIn, loginController.get);
 Router.post("/login", loginController.post, loginController.loginSuccess);
 
+const registerController = require("../controllers/registerController");
+Router.get("/register", isUserNotLoggedIn, registerController.get);
+Router.post("/register", registerController.post);
+
 Router.get("*", (req, res) => {
   res.render("error", { code: 404, msg: "Page not found" });
 });
