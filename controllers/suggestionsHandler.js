73d73828e0058f84ad0b3b5264c9403a70037418
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-09-01 17:37:58 +0430

    suggestion system bug fixed

--- controllers/suggestionsHandler.js
+++ controllers/suggestionsHandler.js
@@ -14,9 +14,9 @@ const post = async (req, res) => {
 
   if (suggestion && req.body.mode == "accept") {
     await axios
-      .post(`http://localhost:3000/sendMsg?id=${suggestion.disId}`, {
-        message: `Ba salam va khaste nabashid. Suggestion shoma tavasote team developer AriDorri barresi va accept shod.\n\n\nMatne suggestion : ${suggestion.text}\n\nBa tashakor az shoma. AriDorri STAFF`,
-      })
+      .post(
+        `http://localhost:3000/sendMsg?id=${suggestion.disId}&message=>>> **Ba salam va khaste nabashid. Suggestion shoma tavasote team developer AriDorri barresi va accept shod.\nMatne suggestion**: ${suggestion.text}\n\n**Ba tashakor az shoma.** AriDorri STAFF`
+      )
       .then(async (result) => {
         if (result.data["status"] == "sent") {
           await suggestion.update({ isAccepted: "yes" });
@@ -31,10 +31,25 @@ const post = async (req, res) => {
         }
       })
       .catch((error) => {});
-  } else if (suggestion && req.body.mode == "declined") {
-    await axios.post(`http://localhost:3000/sendMsg?id=${suggestion.disId}`, {
-      message: `Ba salam va khaste nabashid. Mamnoon az inke suggestion khod ra sabt kardid.\nSuggestion shoma tavasote developer va admin ha barresi shod ama moteasefane rad shod.\n\n\nMatne suggestion : ${suggestion.text}\n\nBa tashakor az shoma. AriDorri STAFF`,
-    });
+  } else if (
+    suggestion &&
+    req.body.mode == "declined" &&
+    suggestion.isAccepted !== "yes"
+  ) {
+    await axios.post(
+      `http://localhost:3000/sendMsg?id=${suggestion.disId}&message=>>> **Ba salam va khaste nabashid. Mamnoon az inke suggestion khod ra sabt kardid.\nSuggestion shoma tavasote developer va admin ha barresi shod ama moteasefane rad shod.\nMatne suggestion**: ${suggestion.text}\n\nBa tashakor az shoma. AriDorri STAFF`
+    );
+    await suggestion.destroy();
+    req.flash("success", "Successfully deleted suggestion.");
+    res.redirect("/suggestions");
+  } else if (
+    suggestion &&
+    req.body.mode == "declined" &&
+    suggestion.isAccepted == "yes"
+  ) {
+    await axios.post(
+      `http://localhost:3000/sendMsg?id=${suggestion.disId}&message=>>> **Ba salam va khaste nabashid. Mamnoon az inke suggestion khod ra sabt kardid.**\nSuggestion shoma tavasote developer takmil shod va ham aknoon dar dastres gharar gereft.\n**Matne suggestion**: ${suggestion.text}\n\nBa tashakor az shoma. AriDorri STAF`
+    );
     await suggestion.destroy();
     req.flash("success", "Successfully deleted suggestion.");
     res.redirect("/suggestions");
