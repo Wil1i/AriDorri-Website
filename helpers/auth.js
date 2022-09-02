df02a4a2668c6a65bf7b29c3e3e5807b9ae70ae1
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-09-02 18:55:07 +0430

    auth system updated

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,59 +1,50 @@
-const User = require("../models/User")
-const config = require("../configs/config.json")
+const User = require("../models/User");
+const config = require("../configs/config.json");
 
 // ?----------- Check if user is logged in
 const isUserLoggedIn = (req, res, next) => {
+  if (!req.user) {
+    req.flash("warning", "برای دیدن این صفحه وارد شوید");
+    req.session.redirectTo = req.url;
+    return res.redirect("/login");
+  }
 
-    if(!req.user){
-
-        req.flash("warning", "You must be logged in to view this page")
-        req.session.redirectTo = req.url;
-        return res.redirect("/login");
-
-    }
-
-    next();
-}
+  next();
+};
 
 // ?----------- Check if user is not logged in
-const isUserNotLoggedIn = (req, res, next) => { 
-
-    if(req.user) return res.redirect("/dashboard")
-    next()
-}
-
-// ?----------- Chcek if user have admin rank
-const isUserAdmin = (req, res, next) => { 
-
-    if(req.user.userRank !== "admin") return res.redirect("/dashboard")
-    next()
-
-}
+const isUserNotLoggedIn = (req, res, next) => {
+  if (req.user) return res.redirect("/dashboard");
+  next();
+};
 
 // ?----------- Chcek if user have moderator or super moderator rank on stream
 const isUserMod = async (req, res, next) => {
+  if (!req.user) {
+    return res.status(403).render("error", config.errors[403]);
+  }
 
-    if(!req.user){
-        return res.status(403).render("error", config.errors[403])
-    }
+  const findUser = await User.findOne({
+    where: {
+      username: req.user.username,
+    },
+  });
 
-    const findUser = await User.User.findOne({
-        where : {
-            username : req.user.username
-        }
-    })
-
-    if(findUser && (findUser.userRank == "mod" || findUser.userRank == "super mod" || findUser.userRank == "developer" || findUser.userRank == "owner")){
-        next()
-    }else{
-        return res.status(403).render("error", config.errors[403])
-    }
-
-}
+  if (
+    findUser &&
+    (findUser.userRank == "mod" ||
+      findUser.userRank == "super mod" ||
+      findUser.userRank == "developer" ||
+      findUser.userRank == "owner")
+  ) {
+    next();
+  } else {
+    return res.status(403).render("error", config.errors[403]);
+  }
+};
 
 module.exports = {
-    isUserLoggedIn,
-    isUserNotLoggedIn,
-    isUserAdmin,
-    isUserMod
-}
+  isUserLoggedIn,
+  isUserNotLoggedIn,
+  isUserMod,
+};
